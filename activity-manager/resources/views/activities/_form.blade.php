8546eb39f7b76e7f3e9150b68b55595e4addb0b0
unknown <fahmihadiwijaya82@gmail.com>
2026-09-25 23:01:31 +0700

    updatee

--- activity-manager/resources/views/activities/_form.blade.php
+++ activity-manager/resources/views/activities/_form.blade.php
@@ -1,39 +1,5 @@
-<div>
-    <label for="title">Judul:</label><br>
-    <input type="text" id="title" name="title" value="{{ old('title', $activity->title ?? '') }}" style="width: 320px;">
-    @error('title')
-        <p style="color: red; margin: 4px 0;">{{ $message }}</p>
-    @enderror
-</div>
-
-<div style="margin-top: 10px;">
-    <label for="description">Deskripsi:</label><br>
-    <textarea id="description" name="description" rows="3" style="width: 320px;">{{ old('description', $activity->description ?? '') }}</textarea>
-    @error('description')
-        <p style="color: red; margin: 4px 0;">{{ $message }}</p>
-    @enderror
-</div>
-
-<div style="margin-top: 10px;">
-    <label for="activity_date">Tanggal:</label><br>
-    <input type="date" id="activity_date" name="activity_date" value="{{ old('activity_date', $activity->activity_date ?? '') }}">
-    @error('activity_date')
-        <p style="color: red; margin: 4px 0;">{{ $message }}</p>
-    @enderror
-</div>
-
-<div style="margin-top: 10px;">
-    <label for="category">Kategori:</label><br>
-    <input type="text" id="category" name="category" value="{{ old('category', $activity->category ?? '') }}">
-    @error('category')
-        <p style="color: red; margin: 4px 0;">{{ $message }}</p>
-    @enderror
-</div>
-
-<div style="margin-top: 10px;">
-    <label for="status">Status:</label><br>
-    <input type="text" id="status" name="status" value="{{ old('status', $activity->status ?? '') }}">
-    @error('status')
-        <p style="color: red; margin: 4px 0;">{{ $message }}</p>
-    @enderror
-</div>
+<x-form-field name="title" label="Judul" />
+<x-form-textarea name="description" label="Deskripsi" />
+<x-form-field name="activity_date" label="Tanggal" type="date" />
+<x-form-field name="category" label="Kategori" />
+<x-form-field name="status" label="Status" />
